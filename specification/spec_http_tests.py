b78eec291d76c8e47b417c5ab472a3a11740ca18
e.pogrebnyak@gmail.com <e.pogrebnyak@gmail.com>
2017-10-04 12:34:24 +0300

    specifiction moved to different folder

--- specification/spec_http_tests.py
+++ specification/spec_http_tests.py
@@ -1,17 +1,30 @@
-import requests
+"""Specification tests for deployed application. 
 
+These tests are a quick check for "Does appliation generally fit the 
+<spec_database.py> specification?"
+
+These tests call http address of deployed apllication. They may be used to check
+both flask and django application.
+   
+These tests DO NOT substitute internal unittests inside the application. 
+The intenal tests should not call http, but rather use app.test_client()
+in flask or similar command in django.
+
+"""
 import json
+
+import requests
 
 HOST = 'http://127.0.0.1:5000'
 POST_URI = HOST + '/api/incoming'
 BASE_URI = HOST + '/api/datapoints'
 
-DATA = json.dumps([{'date': '2017-09-18', 'freq': 'd', 'name': 'BRENT', 'value'    : 55.5},
-                     {'date': '2017-09-15', 'freq': 'd', 'name': 'BRENT', 'value': 56.18},
-                     {'date': '2017-09-14', 'freq': 'd', 'name': 'BRENT', 'value': 56.76},
-                     {'date': '2017-09-13', 'freq': 'd', 'name': 'BRENT', 'value': 55.52},
-                     {'date': '2017-09-12', 'freq': 'd', 'name': 'BRENT', 'value': 55.06},
-                     {'date': '2017-09-11', 'freq': 'd', 'name': 'BRENT', 'value': 54.2}]
+DATA = json.dumps([{'date': '2017-09-18', 'freq': 'd', 'name': 'BRENT', 'value': 55.5},
+                   {'date': '2017-09-15', 'freq': 'd', 'name': 'BRENT', 'value': 56.18},
+                   {'date': '2017-09-14', 'freq': 'd', 'name': 'BRENT', 'value': 56.76},
+                   {'date': '2017-09-13', 'freq': 'd', 'name': 'BRENT', 'value': 55.52},
+                   {'date': '2017-09-12', 'freq': 'd', 'name': 'BRENT', 'value': 55.06},
+                   {'date': '2017-09-11', 'freq': 'd', 'name': 'BRENT', 'value': 54.2}]
 )
 
 
@@ -20,7 +33,6 @@ class TestPOST():
     def test_posting_valid_data(self):
         """Return an empty JSON on success"""
         data = DATA
-
         headers = {'content-type': 'application/json'}
         response = requests.post(POST_URI, json=data, headers=headers)
         assert response.json() == []
@@ -29,6 +41,7 @@ class TestPOST():
     def test_posting_nothing(self):
         """Results in a 400 error"""
         response = requests.post(POST_URI)
+        # FIXME:'123qwe'
         assert response.status_code == '123qwe'
 
 
